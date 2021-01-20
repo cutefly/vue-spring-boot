9a46e0e68c3b1b88276638d8211cc10a20fe189f
chris <chris@kpcard.co.kr>
2021-01-20 19:29:22 +0900

    Set proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,16 @@ const path = require('path')
 
 module.exports = {
   // options...
-    outputDir: path.resolve(__dirname, "../src/main/resources/static")
+  devServer: { // api 요청이 있을때 어디에서 처리할지를 설정
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  },
+  outputDir: path.resolve(__dirname, "../src/main/resources/static")
 }
