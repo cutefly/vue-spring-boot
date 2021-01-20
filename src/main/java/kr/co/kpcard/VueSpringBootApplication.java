513c85972a63ad8725f6731d50b58724cf3ea4ab
chris <chris@kpcard.co.kr>
2021-01-20 18:37:46 +0900

    First commit

---
+++ src/main/java/kr/co/kpcard/VueSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kpcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/kpcard/VueSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kpcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
